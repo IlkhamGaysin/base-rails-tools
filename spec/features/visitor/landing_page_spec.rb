3725a0e96bf829ff9cba1610cf71430acf03fa82
Ilkham Gaysin <ilkham.gaysin@flatstack.com>
2016-10-23 14:58:42 +0300

    Add cats video on main page and test for it (#2)
    finish [#132974601]

--- spec/features/visitor/landing_page_spec.rb
+++ spec/features/visitor/landing_page_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'Landing page' do
   scenario 'Visitor can observe landing page' do
     visit root_path
-    expect(page).to have_content('Hello world')
+    expect(page).to have_content('Cats')
   end
 end
