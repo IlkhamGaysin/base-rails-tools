578227dfb8e7a12963acb6c5876c5c4b6f6c18e6
g-ilham <ilgamgaysin@gmail.com>
2016-11-14 20:37:06 +0300

    Correct quality

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,38 +1,10 @@
-# class Feedback
-#   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-#   include ActiveModel::Model
-
-#   attr_accessor :email, :name, :message
-
-#   validates :email, :name, :message, presence: true
-#   validates :email, format: { with: VALID_EMAIL_REGEX }
-
-#   def attributes=(attributes)
-#     attributes.each do |key, value|
-#       public_send "#{key}=", value
-#     end
-#   end
-# end
-
-
 class Feedback
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   include ActiveModel::Model
 
   attr_accessor :email, :name, :message
 
   validates :email, :name, :message, presence: true
-  validates :email, format: VALID_EMAIL_REGEX
-
-  def save
-    valid?
-  end
-
-  def attributes=(attributes)
-    attributes.each do |key, value|
-      public_send "#{key}=", value
-    end
-  end
+  validates :email, format: { with: VALID_EMAIL_REGEX }
 end
--- spec/features/visitor/create_feedback_spec.rb
+++ spec/features/visitor/create_feedback_spec.rb
@@ -1,23 +1,23 @@
-require "rails_helper"
+require 'rails_helper'
 
-feature "Create Feedback" do
+feature 'Create Feedback' do
   let!(:feedback_attributes) { attributes_for(:feedback) }
 
-  scenario "Visitor creates feedback" do
+  scenario 'Visitor creates feedback' do
     visit new_feedback_path
 
     fill_form :feedback, feedback_attributes
-    click_button "Submit"
+    click_button 'Submit'
 
-    open_email(ENV.fetch("MAILER_SENDER_ADDRESS"))
+    open_email(ENV.fetch('MAILER_SENDER_ADDRESS'))
 
-    expect(current_email).to have_subject("Feedback")
+    expect(current_email).to have_subject('Feedback')
     expect(current_email).to be_delivered_from(feedback_attributes[:email])
 
     expect(current_email).to have_body_text(feedback_attributes[:name])
     expect(current_email).to have_body_text(feedback_attributes[:email])
     expect(current_email).to have_body_text(feedback_attributes[:message])
 
-    expect(page).to have_content("Email was successfully sent.")
+    expect(page).to have_content('Email was successfully sent.')
   end
 end
